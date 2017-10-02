091985f72bdbc135f8dd4c917e29a12ab8068d0a
Bruce Thomson <bwt3@hw.ac.uk>
2017-10-02 19:05:25 +0100

    Stuff

---
+++ ADCTest/ADCTestADC/ADCTestADC_inst.vhd
@@ -0,0 +1,13 @@
+	component ADCTestADC is
+		port (
+			clk_clk       : in std_logic := 'X'; -- clk
+			reset_reset_n : in std_logic := 'X'  -- reset_n
+		);
+	end component ADCTestADC;
+
+	u0 : component ADCTestADC
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
+			reset_reset_n => CONNECTED_TO_reset_reset_n  -- reset.reset_n
+		);
+
